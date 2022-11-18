138b48be51e54aed6d81d446d43cef6e3e2a677f
4kang <106650699+4kang@users.noreply.github.com>
2022-11-18 12:50:29 +0000

    Add php

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
-<?php echo 'Hello World'; ?>
-
-<?php echo 5 + 3 ; ?>
-
-<?php
- $name = 'ジュン';
- echo $name ;
-?>
-
-<!--省略-->
-<?php $names = 'にんじゃ'; $names .= 'わんこ'; echo $names; ?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8" />
+  <title>お問い合わせフォーム</title>
+  <link rel="stylesheet" href="style.css">
+ </head>
+ 
+ <header>
+  <div class="header-left">DMM WEBCAMP</div>
+   <div class="header-right">
+    <ul>
+     <li>会社概要</li>
+     <li>採用</li>
+     <li class="selected">お問い合わせ</li>
+    </ul>
+   </div>
+ </header>
+ 
+ <body>
+  <div class="main">
+   <div class="contact-form">
+    <div class="form-title">お問い合わせ</div>
+    
+    <!--フォーム作成のためformタグで囲う-->
+    <form method="post" action="sent.php">
+     <div class="form-item">名前</div>
+     <input type="text" name="name">
+     <div class="form-item">内容</div>
+     <textarea name="body"></textarea>
+    </form>
+   </div>
+  </div>
+  
+  <footer>
+   <div class="footer-left">
+    <ul>
+     <li>会社概要</li>
+     <li>採用</li>
+     <li>お問い合わせ</li>
+    </ul>
+   </div>
+  </footer>
+  
+ </body>
+</html>
